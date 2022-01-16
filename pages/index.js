b507e542981ae178f00d4db86b4ec5f13f427321
Alan W. Smith <alan@alanwsmith.com>
2022-01-16 16:23:56 -0500

    Updated text on home page

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,11 @@ export default function Page() {
           <a>How-To Use D3 In Next.js</a>
         </Link>
       </p>
+      <p>
+        There&apos;s obviously not much here right now. I mainly built it to
+        figure out the Next.js Sunburst Sequence. I&apos;ll add to it over time
+        when I come up with new things to mess with
+      </p>
       <ul>
         {links.map((link) => (
           <li key={link}>
